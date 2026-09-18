ccb77575616ad23d2f1e0c03143dcaa502094966
dreikovich <andrzejhk@gmail.com>
2026-09-18 21:30:49 +0200

    Add Blazor Server web project with interactive restaurant page

---
+++ RestaurantSimulation.Web/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace RestaurantSimulation.Web.Components.Layout;
+
+public partial class MainLayout : LayoutComponentBase
+{
+}
---
+++ RestaurantSimulation.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace RestaurantSimulation.Web.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+}
